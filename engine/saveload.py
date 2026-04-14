0fe37bc6b6ee4feefdfa07958c90c6bb63503c3a
Nathan <149914029+Natuworkguy@users.noreply.github.com>
2026-04-14 15:39:14 -0400

    Added "# noqa" to type checker ignore statement to work on all platforms

--- engine/saveload.py
+++ engine/saveload.py
@@ -10,7 +10,7 @@ import os
 
 def resource_path(relative: str) -> str:
     if hasattr(sys, "_MEIPASS"):
-        return os.path.join(sys._MEIPASS, relative) # pyright: ignore[reportAttributeAccessIssue]
+        return os.path.join(sys._MEIPASS, relative) # pyright: ignore[reportAttributeAccessIssue] # noqa
     return os.path.join(os.path.abspath("."), relative)
 
 
