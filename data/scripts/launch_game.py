1a04dc7421332f1f7e8e0f812cb8a894de2e3d9c
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-04-23 23:36:04 -0400

    Add space before inline comment in data/scripts/launch_game.py

--- data/scripts/launch_game.py
+++ data/scripts/launch_game.py
@@ -15,7 +15,7 @@ PROJECT_FILE = "game.absp"
 
 def resource_path(relative: str) -> str:
     if hasattr(sys, "_MEIPASS"):
-        return os.path.join(sys._MEIPASS, relative) # pyright: ignore[reportAttributeAccessIssue]
+        return os.path.join(sys._MEIPASS, relative)  # pyright: ignore[reportAttributeAccessIssue]
     return os.path.join(os.path.abspath("."), relative)
 
 def game_path(relative: str) -> str:
