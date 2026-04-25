bee95974fbc0802d2d894e9aba62b50557e2afcf
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-04-25 00:00:42 -0400

    Update theme.tcl: Add floating Labelframes and get_color procedure
    Updated Labelframe colors to look floating.
    Colors now sourced from ui_color dictionary.
    
    Added get_color proc:
    Supplied string: color_name
    Checks color_name against ui_colors dictionary
    If color exists: returns color's hex code
    If color does not exist: Raises error: "get_color: Invalid color name: \"$color_name\""

--- engine/tcl/theme.tcl
+++ engine/tcl/theme.tcl
@@ -1,8 +1,37 @@
-set abs_font [list "Segoe UI" 10]
+set font [dict create]
 
-option add *Font               $abs_font
-option add *Button.Font        $abs_font
-option add *Label.Font         $abs_font
-option add *Entry.Font         $abs_font
-option add *selectedBackground #0078D7
-option add *selectedForeground #FFFFFF
+dict set font default [list "Segoe UI" 10]
+dict set font small   [list "Segoe UI" 9]
+
+set ui_color [dict create]
+
+dict set ui_color background          #FFFFFF
+dict set ui_color selected_background #0078D7
+dict set ui_color text                #333333
+dict set ui_color selected_foreground #FFFFFF
+
+option add *foreground              [dict get $ui_color text]
+option add *background              [dict get $ui_color background]
+option add *selectedBackground      [dict get $ui_color selected_background]
+option add *selectedForeground      [dict get $ui_color selected_foreground]
+
+option add *Font                    [dict get $font default]
+option add *Button.Font             [dict get $font default]
+option add *Label.Font              [dict get $font default]
+option add *Entry.Font              [dict get $font default]
+option add *LabelFrame.Font         [dict get $font small]
+option add *LabelFrame.foreground   [dict get $ui_color text]
+option add *LabelFrame.background   [dict get $ui_color background]
+
+option add *LabelFrame.relief       solid
+
+
+proc get_color {color_name} {
+    global ui_color
+
+    if {[dict exists $ui_color $color_name]} {
+        return [dict get $ui_color $color_name]
+    }
+
+    error "get_color: Invalid color name: \"$color_name\""
+}
