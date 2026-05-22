57a6d4e654a2754dfee74d04457c7557869a398c
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-05-22 02:56:52 -0400

    Add Flake8 E501 ignore statement to launch_game.py

--- data/scripts/launch_game.py
+++ data/scripts/launch_game.py
@@ -19,7 +19,7 @@ GP_BASE_PATH = Path(__file__).parent
 
 def resource_path(relative: str) -> str:
     if hasattr(sys, "_MEIPASS"):
-        return os.path.join(sys._MEIPASS, relative)  # pyright: ignore[reportAttributeAccessIssue]
+        return os.path.join(sys._MEIPASS, relative)  # pyright: ignore[reportAttributeAccessIssue] # noqa: E501
     return os.path.join(os.path.abspath("."), relative)
 
 
