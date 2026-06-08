4d688411af6322691bd601fc8a5d0579301b0871
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-06-07 22:59:24 -0400

    Document shared types module

--- engine/core/types.py
+++ engine/core/types.py
@@ -1,6 +1,8 @@
 # Copyright (C) Natuworkguy
 # See the LICENSE file for GPLv3
 
+"""Shared type aliases for ABS Engine."""
+
 from typing import TypeAlias
 
 RGBType: TypeAlias = tuple[int, int, int]
