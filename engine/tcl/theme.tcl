a39963a77085e272a7c70613585c3097b509d400
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-06-16 18:54:31 -0400

    Remove background color

--- engine/tcl/theme.tcl
+++ engine/tcl/theme.tcl
@@ -5,13 +5,10 @@ dict set font small   [list "Segoe UI" 9]
 
 set ui_color [dict create]
 
-dict set ui_color background          #FFFFFF
 dict set ui_color selected_background #0078D7
 dict set ui_color text                #333333
 dict set ui_color selected_foreground #FFFFFF
 
-option add *foreground              [dict get $ui_color text]
-option add *background              [dict get $ui_color background]
 option add *selectedBackground      [dict get $ui_color selected_background]
 option add *selectedForeground      [dict get $ui_color selected_foreground]
 
@@ -20,7 +17,5 @@ option add *Button.Font             [dict get $font default]
 option add *Label.Font              [dict get $font default]
 option add *Entry.Font              [dict get $font default]
 option add *LabelFrame.Font         [dict get $font small]
-option add *LabelFrame.foreground   [dict get $ui_color text]
-option add *LabelFrame.background   [dict get $ui_color background]
 
 option add *LabelFrame.relief       solid
