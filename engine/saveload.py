39f47daf12ef5febc6b901efaacd7194f1f6e833
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-06-16 00:06:33 -0400

    Add docstring to resource_path()

--- engine/saveload.py
+++ engine/saveload.py
@@ -10,6 +10,16 @@ import os
 
 
 def resource_path(relative: str) -> str:
+    """
+    Convert a relative resource path into an absolute path.
+
+    Args:
+        relative (str): Relative path to a resource.
+
+    Returns:
+        str: Absolute path to the resource.
+    """
+
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, relative)  # pyright: ignore[reportAttributeAccessIssue]
     return os.path.join(os.path.abspath("."), relative)
