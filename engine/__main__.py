97188e81bea161d05715db85c616570dacda4dcd
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-06-17 01:13:37 -0400

    Add docstring to __main__.py

--- engine/__main__.py
+++ engine/__main__.py
@@ -1,6 +1,10 @@
 # Copyright (C) Natuworkguy
 # See the LICENSE file for GPLv3
 
+"""
+ABS Engine entry module.
+"""
+
 from .logger import Status, logger
 
 logger("Use run.pyw to start ABS Engine.", status=Status.CRITICAL)
