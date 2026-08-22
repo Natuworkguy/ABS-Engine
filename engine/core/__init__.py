9728002b3261953ba6971b5982fd125202e85203
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-08-22 13:25:27 -0700

    Prevent title setting in non-editor mode when stdout is a terminal

--- engine/core/__init__.py
+++ engine/core/__init__.py
@@ -432,7 +432,7 @@ class Game:
         self.screen: pygame.Surface = pygame.display.set_mode(self.wsize, display_flags)
         pygame.display.set_caption(title)
 
-        if sys.stdout.isatty():
+        if not IS_EDITOR and sys.stdout.isatty():
             print(colorama.ansi.set_title(title), end="")
 
         self.set_icon(icon_path)
