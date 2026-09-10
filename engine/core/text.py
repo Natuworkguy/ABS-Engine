f8f62784cc30f700ad4da8929995afa035d0675b
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-09-09 21:20:54 -0700

    Add color parameter to superclass initialization in Text entity

--- engine/core/text.py
+++ engine/core/text.py
@@ -66,7 +66,7 @@ class Text(Entity):
 
         self._update_text_surface()
 
-        super().__init__(x, y, width=self.text_rect.width, height=self.text_rect.height)
+        super().__init__(x, y, width=self.text_rect.width, height=self.text_rect.height, color=self.color)
 
     def _update_text_surface(self) -> None:
         """
