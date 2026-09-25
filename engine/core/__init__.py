4c0b361de42399a6548bc3d510150cef665eff45
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2026-09-24 22:05:41 -0700

    Set SDL_APP_NAME environment variable for window title in Game initialization

--- engine/core/__init__.py
+++ engine/core/__init__.py
@@ -476,8 +476,10 @@ class Game:
         self.screen: pygame.Surface = pygame.display.set_mode(self.wsize, display_flags)
         pygame.display.set_caption(title)
 
-        if not IS_EDITOR and sys.stdout is not None and sys.stdout.isatty():
-            print(colorama.ansi.set_title(title), end="")
+        if not IS_EDITOR:
+            os.environ["SDL_APP_NAME"] = title
+            if sys.stdout is not None and sys.stdout.isatty():
+                print(colorama.ansi.set_title(title), end="")
 
         if icon_path is not None:
             self.set_icon(icon_path)
